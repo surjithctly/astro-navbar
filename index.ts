1fe322382892c3cdfc1a1593ec462e75c308e758
Surjith S M <surjithctly@gmail.com>
2023-01-03 22:02:43 +0530

    add OpenIcon & CloseIcon fixes #1

--- index.ts
+++ index.ts
@@ -1,5 +1,7 @@
 export { default as Astronav } from "./src/Navbar.astro";
-export { default as MenuIcon }  from "./src/components/MenuIcon.astro";
-export { default as MenuItems }  from "./src/components/MenuItems.astro";
-export { default as Dropdown }  from "./src/components/Dropdown.astro";
-export { default as DropdownItems }  from "./src/components/DropdownItems.astro";
+export { default as MenuIcon } from "./src/components/MenuIcon.astro";
+export { default as OpenIcon } from "./src/components/OpenIcon.astro";
+export { default as CloseIcon } from "./src/components/CloseIcon.astro";
+export { default as MenuItems } from "./src/components/MenuItems.astro";
+export { default as Dropdown } from "./src/components/Dropdown.astro";
+export { default as DropdownItems } from "./src/components/DropdownItems.astro";
